6f3eb5b27b50872b3473ed8e60b26fb8c9fcdbdc
MatriMariem <meriemmatri1994@gmail.com>
2023-06-05 16:34:04 +0100

    Task 0: Structure Complex

---
+++ 0x00-math_complex/0-display.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * display_complex_number - displays a complex number
+ * @c: a complex number
+ */
+
+void display_complex_number(complex c)
+{
+	if (c.im < 0)
+	{
+		printf("%.0f - %.0fi\n", c.re, c.im);
+	}
+	else if (c.im == 0)
+	{
+		printf("%.0f\n", c.re);
+	}
+	else
+	{
+		printf("%.0f + %.0fi\n", c.re, c.im);
+	}
+
+}
---
+++ 0x00-math_complex/holberton.h
@@ -0,0 +1,13 @@
+#ifndef COMPLEX_H
+#define COMPLEX_H
+
+
+typedef struct complex
+{
+	double re;
+	double im;
+} complex;
+
+void display_complex_number(complex c);
+
+#endif
