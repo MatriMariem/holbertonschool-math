ba67f4c2cbe3121e0db071345197d1eafdd2b533
MatriMariem <meriemmatri1994@gmail.com>
2023-06-05 23:49:28 +0100

    handle edge cases

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -8,17 +8,61 @@
 
 void display_complex_number(complex c)
 {
-	if (c.im < 0)
+	if (c.re != 0)
 	{
-		printf("%.0f - %.0fi\n", c.re, c.im);
+		printf("%.0f", c.re);
 	}
-	else if (c.im == 0)
+
+	if (c.im == -1)
 	{
-		printf("%.0f\n", c.re);
+		if (c.re != 0)
+		{
+			printf(" - i");
+		}
+		else
+		{
+			printf("-i");
+		}
 	}
-	else
+	else if (c.im == 1)
 	{
-		printf("%.0f + %.0fi\n", c.re, c.im);
+		if (c.re != 0)
+		{
+			printf(" + i");
+		}
+		else
+		{
+			printf("i");
+		}
 	}
+
+	else if (c.im < 0)
+	{
+		if (c.re != 0)
+		{
+			printf(" - %.0fi", -c.im);
+		}
+		else
+		{
+			printf("%.0fi", c.im);
+		}
+	}
+	else if (c.im > 0)
+	{
+		if (c.re != 0)
+		{
+			printf(" + %.0fi", c.im);
+		}
+		else
+		{
+			printf("%.0fi", c.im);
+		}
+	}
+	else if (c.re == 0 && c.im == 0)
+	{
+		printf("0");
+	}
+
+	printf("\n");
 
 }
